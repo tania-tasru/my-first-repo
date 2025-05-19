e50c587f8a809e4f0a23499ba88ed047e0b5ded5
tania-tasru <tasrutania@gmail.com>
2025-05-19 22:07:44 +0600

    Create star pattern.py

---
+++ star pattern.py
@@ -0,0 +1,16 @@
+#Star_Pattern
+n=4
+for i in range(n+1):
+    print(i*"★")
+    
+n=6
+for i in range(n+1):
+    print((2*i-1)*"★")
+    
+n=int(input("Enter the number: "))
+for i in range(n+1):
+    print(i*"★")
+    
+n=int(input("Enter the number: "))
+for i in range(n+1):
+    print((2*i-1)*"★")
